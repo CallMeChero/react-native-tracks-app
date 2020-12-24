c61a0ae0f56f3a0a61cb75333801773f87ef0468
CallMeChero <david.ceranic@gmail.com>
2020-12-24 11:02:13 +0100

    Initial commit with screen boilerplates and initial navigation

---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignUpScreen = () => {
+    return <Text style={{ fontSize: 48}}>SignUpScreen</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default SignUpScreen;
