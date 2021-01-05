81f3a13a2b02b6b4dc96a6ff6daab2e1a5501bc1
CallMeChero <david.ceranic@gmail.com>
2021-01-05 13:26:30 +0100

    Add context & initial implementation of signup component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, Header } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
 import SignInScreen from './src/screens/SignInScreen';
@@ -8,11 +8,39 @@ import SignUpScreen from './src/screens/SignUpScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Provider as AuthProvider } from './src/context/AuthContext';
+
+const globalNavigationOptions = {
+  // title: `Sign in`,
+  headerBackTitle: 'Nazad',
+  headerBackground: () => (
+    <LinearGradient
+      colors={['#a13388', '#10356c']}
+      style={{ flex: 1 }}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 0}}
+    />
+  ),
+  headerTitleStyle: {
+    color: '#fff'
+  }
+}
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
-    SignUp: SignUpScreen,
-    SignIn: SignInScreen
+    SignUp: {
+      screen: SignUpScreen,
+      navigationOptions: () => {
+        return globalNavigationOptions
+      }
+    },
+    SignIn: {
+      screen: SignInScreen,
+      navigationOptions: () => {
+        return globalNavigationOptions
+      }
+    },
   }),
   mainFlow: createBottomTabNavigator({
     trackListFlow: createStackNavigator({
@@ -24,4 +52,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+}
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignUpScreen = () => {
-    return <Text style={{ fontSize: 48}}>SignUpScreen</Text>
+const SignUpScreen = ({navigation}) => {
+
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    return <View style={styles.container}>
+        <Spacer>
+            <Text h3>
+                Sign Up for Tracker
+            </Text>
+        </Spacer>
+        <Input 
+            label="Email" 
+            value={email} 
+            onChangeText={(newEmail) => setEmail(newEmail)}
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
+        <Spacer />
+        <Input
+            secureTextEntry={true}
+            label="Password" 
+            value={password} 
+            onChangeText={(newPassword) => setPassword(newPassword)}
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
+        <Spacer>
+            <Button title="Sign up"/>
+        </Spacer>
+    </View>;
 }
 
-const styles = StyleSheet.create({});
+SignUpScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+//alternative
+// SignUpScreen.navigationOptions = {
+//     headerShown: false
+// }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:'center',
+        marginBottom: 250
+    }
+});
 
 export default SignUpScreen;
