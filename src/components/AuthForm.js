2a5b270609b57ddc382cd125b3651a78376ce651
CallMeChero <david.ceranic@gmail.com>
2021-01-06 14:23:28 +0100

    Make reusable AuthForm & NavLinks component for Auth components

---
+++ src/components/AuthForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, Button, Input } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+
+const AuthForm = ({ headerText, errorMessage, onSubmit, submitText }) => {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    return (
+        <>
+            <Spacer>
+                <Text h3>{headerText}</Text>
+            </Spacer>
+            <Input 
+                label="Email" 
+                value={email} 
+                onChangeText={(newEmail) => setEmail(newEmail)}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Spacer />
+            <Input
+                secureTextEntry={true}
+                label="Password" 
+                value={password} 
+                onChangeText={(newPassword) => setPassword(newPassword)}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            {errorMessage ? <Text style={styles.errorMessage}>{ errorMessage }</Text> : null}
+            <Spacer>
+                <Button title={submitText} onPress={() => onSubmit({ email, password})}/>
+            </Spacer>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    errorMessage: {
+        fontSize: 16,
+        color: 'crimson',
+        marginLeft: 15,
+        marginTop: 15
+    }
+});
+
+export default AuthForm;
